3dd480a0e2a93ee4f8212fbc90d0c335e4409224
natnaelh14 <natnaelh14@github.com>
2022-07-11 13:58:31 -0400

    changed button text

--- src/components/ButtonText.tsx
+++ src/components/ButtonText.tsx
@@ -10,9 +10,9 @@ export const ButtonText = ({
     ...props
   }: IButtonText) => {
     return (
-      <button {...props}>
+      <div {...props}>
         <p className="text-left text-sm">{title}</p>
         <p className="text-left text-lg pt-1.5 text-blue-200 font-bold">{subTitle}</p>
-      </button>
+      </div>
     );
   };
